d0bed97b5acdb1226b744ca1424afd4c6bd0429c
Saurabh Belgaonkar <saurabhbelgaonkar@tamu.edu>
2025-03-10 14:00:43 -0500

    "Working code for BOA*"

--- cont_space_approch/older_not_useful/utils.py
+++ cont_space_approch/older_not_useful/utils.py
@@ -3,6 +3,9 @@ from matplotlib.patches import Circle
 import sympy
 from sympy import symbols, Eq, solve
 from sympy import simplify
+import networkx as nx
+import numpy as np
+import networkx as nx
 
 class plot_map:
     
@@ -24,7 +27,6 @@ class plot_map:
         
         for circle_info in self.Map_qz:
             # Create a circle patch
-            print(circle_info)
             circle = Circle((circle_info[0], circle_info[1]), radius=circle_info[2], fill=False, edgecolor='blue', linewidth=2)
 
             # Add the circle to the Axes
@@ -33,7 +35,7 @@ class plot_map:
         if tangents:
             # Plot the tangent lines
             for tangent_point in tangent_points:
-                # print(tangent_point)
+                print(tangent_point)
                 ax.plot( (tangent_point[0][0], tangent_point[1][0]), (tangent_point[0][1], tangent_point[1][1]), 'b--')
                 # ax.plot( (tangent_point[2][0], tangent_point[3][0]), (tangent_point[2][1], tangent_point[3][1]), 'b--')
                 ax.plot( (tangent_point[0][0], tangent_point[1][0]), (tangent_point[0][1], tangent_point[1][1]), 'bo')
@@ -52,15 +54,54 @@ class plot_map:
         # Add a legend
         ax.legend()
         plt.title("Circle Using Patches")
+        plt.grid()
         plt.show()
 
+    def plot_path(self, path_points= [(0,0)]):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(20, 10)
+        
+        ### find the min and max x and y values, for axis limit 
+        min_x = min( [min(self.Map_qz, key=lambda x: x[0]-x[2])[0] - min(self.Map_qz, key=lambda x: x[0]-x[2])[2], self.start[0], self.goal[0]] )
+        max_x = max( [max(self.Map_qz, key=lambda x: x[0]+x[2])[0] + max(self.Map_qz, key=lambda x: x[0]+x[2])[2], self.start[0], self.goal[0]] )
+        min_y = min( [min(self.Map_qz, key=lambda x: x[1]-x[2])[1] - min(self.Map_qz, key=lambda x: x[1]-x[2])[2], self.start[1], self.goal[1]] )
+        max_y = max( [max(self.Map_qz, key=lambda x: x[1]+x[2])[1] + max(self.Map_qz, key=lambda x: x[1]+x[2])[2], self.start[1], self.goal[1]] )
+        
+        
+        for circle_info in self.Map_qz:
+            # Create a circle patch
+            circle = Circle((circle_info[0], circle_info[1]), radius=circle_info[2], fill=False, edgecolor='blue', linewidth=2)
 
-class tangent_line:
+            # Add the circle to the Axes
+            ax.add_patch(circle)
+
+        x_coor = path_points[0]
+        y_coor = path_points[1]
+        ax.plot( x_coor, y_coor, 'b--')
+        # Set equal aspect so circles look circular
+        ax.set_aspect('equal', 'box')        
+        
+        # Set axis limits
+        ax.set_xlim(min_x - (max_x - min_x)/10, max_x + (max_x - min_x)/10)
+        ax.set_ylim(min_y - (max_y - min_y)/10, max_y + (max_y - min_y)/10)
+
+        # Plot the start and goal points
+        ax.plot(self.start[0], self.start[1], 'ro', label='Start')
+        ax.plot(self.goal[0], self.goal[1], 'go', label='Goal')
+
+        # Add a legend
+        ax.legend()
+        plt.title("Circle Using Patches")
+        plt.grid()
+        plt.show()
     
-    def __init__(self, Map_qz, start, goal):
+class graph_construction:
+    
+    def __init__(self, Map_qz, start, goal, discritization_factor=10):
         self.Map_qz = Map_qz
         self.start = start
         self.goal = goal
+        self.discritization_factor = discritization_factor
         
 
     def tangent_from_pt_to_circle(self, point, circle):
@@ -224,12 +265,14 @@ class tangent_line:
             
         return []
     
-    def tangent_from_multiple_points_to_circles(self, points):
+    def Finding_tangents(self):
         """
         Generates tangent lines from 'point' to every circle in self.Map_qz,
         then checks intersections and possibly adjusts them.
         """
         tangents_and_circles_list = []
+        points = [self.start, self.goal]
+        ### Finding the tangents from start and goal to circles
         for point in points:
             # print(f"points: {point}")
             for circle in self.Map_qz:
@@ -248,6 +291,14 @@ class tangent_line:
                 # print(f"circle: {circle} and tangent: {tangent_lines}")
                 if all(len(tangent_line) != 0 for tangent_line in tangent_lines):
                     tangents_and_circles_list.append(tangent_lines)
+        
+        ### Finding the tangents between circles in self.Map_qz
+        for num1, circle1 in enumerate(self.Map_qz):
+            for num2, circle2 in enumerate(self.Map_qz):
+                if num1 < num2:
+                    common_tangents = self.common_tangents(circle1, circle2)
+                    if common_tangents:
+                        tangents_and_circles_list.append(common_tangents)
         
         return tangents_and_circles_list
     
@@ -339,18 +390,179 @@ class tangent_line:
         
         return tangents
 
-### convert to parametetic form, i.e. theta
-### Assume anticlockwise rotation like in normal axis
-#   y
-#   ↑
-#   |
-#   |         
-#   |__________→ x
+    def create_visibility_graph(self, tangents_and_circles_list, index_map, reverse_index_map):
+        
+        Visibility_Graph = nx.DiGraph()
 
-def xy_to_parametric_form(circle, point):
-    x,y,r = circle[0], circle[1], circle[2]
-    # print((point[1]-y)/(point[0]-x))
-    theta = sympy.simplify(sympy.atan2((point[1]-y),(point[0]-x)))
-    theta = theta if theta>= 0 else theta + 2 * sympy.pi
-    # print(theta)
-    return theta
+        ### Case 1, direct start to goal
+        ### Line eq for line between start and goal
+        a, b = (self.start[1]-self.goal[1])/(self.start[0] - self.goal[0]), -1
+        c = self.start[1] - a*self.start[0]
+        line = {"a": a, "b": b, "c": c}
+
+        if not self.tangent_intersection(line):
+            Visibility_Graph.add_edge("s", "g", circle1= (self.start,self.start), circle2= (self.goal,self.goal), edge_type="external")
+
+        ### Case 2, edge between point and a circle
+        for tangents in tangents_and_circles_list:
+            if len(tangents) == 2:
+                point1, point2 = tangents[0]["tangent_point_circle2"], tangents[1]["tangent_point_circle2"]
+                circle1_index, circle2_index = reverse_index_map[tangents[0]["circle1"]], reverse_index_map[tangents[0]["circle2"]]
+                min_max_coordinates1 = {"x_max": tangents[0]["circle1"][0], "y_max": tangents[0]["circle1"][1], "x_min": tangents[0]["circle1"][0], "y_min": tangents[0]["circle1"][1]}
+                min_max_coordinates2 = self.Max_min_coor(tangents[0]["circle2"], (point1, point2))
+                
+                if circle1_index == "s":
+                    circle2_index = str(circle2_index)+ "_i"
+                    Visibility_Graph.add_edge(circle1_index, circle2_index, circle1= (tangents[0]["circle1"],tangents[0]["circle1"]), circle2= (point1,point2), min_max_coordinates1=min_max_coordinates1, min_max_coordinates2=min_max_coordinates2, edge_type="external")
+                elif circle1_index == "g":
+                    circle2_index = str(circle2_index)+ "_o"
+                    Visibility_Graph.add_edge(circle2_index, circle1_index, circle1= (point1,point2), circle2= (tangents[0]["circle1"],tangents[0]["circle1"]), min_max_coordinates1=min_max_coordinates2, min_max_coordinates2=min_max_coordinates1, edge_type="external")
+                # elif circle2_index == "s":
+                #     circle1_index = str(circle1_index)+ "_i"
+                #     Visibility_Graph.add_edge(circle2_index, circle1_index, circle2= (point1,point2), circle1= (tangents[0]["circle1"],tangents[0]["circle1"]), min_max_coordinates1=min_max_coordinates2, min_max_coordinates2=min_max_coordinates1, edge_type="external")
+                # elif circle2_index == "g":
+                #     circle1_index = str(circle1_index)+ "_o"
+                #     Visibility_Graph.add_edge(circle1_index, circle2_index, circle1= (tangents[0]["circle1"],tangents[0]["circle1"]), circle2= (point1,point2),  min_max_coordinates1=min_max_coordinates1, min_max_coordinates2=min_max_coordinates2, edge_type="external")
+
+        ### Case 3, edge between two circles
+        for tangents in tangents_and_circles_list:
+            if len(tangents) == 4:
+                k, l = [ i for i, tangent in enumerate(tangents) if tangent["tangent_type"] == "transverse"]
+                point1_1, point1_2 = tangents[k]["tangent_point_circle1"], tangents[l]["tangent_point_circle1"]
+                point2_1, point2_2 = tangents[k]["tangent_point_circle2"], tangents[l]["tangent_point_circle2"]
+                circle1_index, circle2_index = reverse_index_map[tangents[2]["circle1"]], reverse_index_map[tangents[2]["circle2"]]
+                
+                min_max_coordinates1 = self.Max_min_coor(tangents[0]["circle1"], (point1_1, point1_2))
+                min_max_coordinates2 = self.Max_min_coor(tangents[0]["circle2"], (point2_1, point2_2))
+                
+                Visibility_Graph.add_edge(str(circle1_index)+ "_o", str(circle2_index)+ "_i", circle1= (point1_1,point1_2), circle2= (point2_1,point2_2), min_max_coordinates1=min_max_coordinates1, min_max_coordinates2=min_max_coordinates2, edge_type="external")
+                Visibility_Graph.add_edge(str(circle2_index)+ "_o", str(circle1_index)+ "_i", circle2= (point2_1,point2_2), circle1= (point1_1,point1_2), min_max_coordinates2=min_max_coordinates2, min_max_coordinates1=min_max_coordinates1, edge_type="external")
+
+        ### Case 4, internal edges
+        for index in index_map:
+            if index != "s" and index != "g":
+                if index[-1:] == "i":
+                    index_in = index[:-2] + "_i"
+                    index_out = index[:-2]+ "_o"
+                    Visibility_Graph.add_edge(index_in, index_out, edge_type="internal")
+        
+        external_edges, internal_edges = [], []
+
+        for u, v, data in Visibility_Graph.edges(data=True):
+            if data['edge_type'] == "external":
+                external_edges.append((u, v))
+            else:
+                internal_edges.append((u, v))
+        all_edges = external_edges + internal_edges
+        
+        
+        discrete_graph = nx.DiGraph()
+
+        for u,v in internal_edges:
+            risk_list, pen_dist_list, length_chord_list = self.compute_risk_cost(index_map[u])
+            for i in range(self.discritization_factor):
+                discrete_graph.add_edge(u, i, Risk_cost=risk_list[i], pen_dist = pen_dist_list[i], len_chord = length_chord_list[i])
+                
+        discrete_edges_circle = [ (u,v) for u,v in discrete_graph.edges()]
+
+        return Visibility_Graph, external_edges, internal_edges, all_edges, discrete_graph, discrete_edges_circle
+
+    def normalize_angle(self, theta):
+        """Normalize angle to be within [0, 2*pi)."""
+        return theta % (2 * sympy.pi)
+
+    def angle_between(self, angle, start, end):
+        """
+        Check if angle theta lies on the arc from start to end.
+        Assumes that arc length (end - start mod 2*pi) is the minor arc.
+        """
+        theta = self.normalize_angle(angle)
+        start = self.normalize_angle(start)
+        end = self.normalize_angle(end)
+        
+        # If the arc doesn't cross 0
+        if start <= end:
+            return start <= theta <= end
+        else:
+            # Arc crosses 0, so it's between start and 2pi or 0 and end.
+            return theta >= start or theta <= end
+    
+    def Max_min_coor(self, circle, points):
+        # Compute angles for the two points relative to the circle's center
+        theta1 = self.xy_to_parametric_form(circle, points[0])
+        theta2 = self.xy_to_parametric_form(circle, points[1])
+
+        diff = self.normalize_angle(theta2-theta1)
+        
+        if diff > sympy.pi:
+            start, end = theta2, theta1
+        else:
+            start, end = theta1, theta2
+        
+        # List of candidate angles: endpoints and cardinal angles
+        candidate_angles = [theta1, theta2]
+        
+        # Cardinal angles where x or y extreme occur.
+        cardinals = [0, sympy.pi/2, sympy.pi, 3*sympy.pi/2]
+        
+        for angle in cardinals:
+            if self.angle_between(angle, start, end):
+                candidate_angles.append(angle)
+        
+        # Compute the x,y coordinates for all candidate angles and get bounding box
+        xs, ys = [], []
+        for theta in candidate_angles:
+            x = circle[0] + circle[2] * sympy.cos(theta)
+            y = circle[1] + circle[2] * sympy.sin(theta)
+            xs.append(x)
+            ys.append(y)
+        
+        return { "x_min": min(xs), "y_min":min(ys), "x_max": max(xs), "y_max": max(ys) }
+    
+    ### convert to parametetic form, i.e. theta
+    ### Assume anticlockwise rotation like in normal axis
+    #   y
+    #   ↑
+    #   |
+    #   |         
+    #   |__________→ x
+    def xy_to_parametric_form(self, circle, point):
+        x,y,r = circle[0], circle[1], circle[2]
+        # print((point[1]-y)/(point[0]-x))
+        theta = sympy.simplify(sympy.atan2((point[1]-y),(point[0]-x)))
+        theta = theta if theta>= 0 else theta + 2 * sympy.pi
+        # print(theta)
+        return theta
+
+    ### Mapping from index to actual values
+    def create_index_map(self):
+        index_map = {"s": (self.start[0], self.start[1],0), "g": (self.goal[0], self.goal[1], 0)}
+
+        for i, circle in enumerate(self.Map_qz):
+            index_in = str(i+1)+ "_i"
+            index_out = str(i+1)+ "_o"
+            index_map[index_in] = circle
+            index_map[index_out] = circle
+
+        ### Reverse index map
+        index_map_simple = {"s": (self.start[0], self.start[1],0), "g": (self.goal[0], self.goal[1], 0)}
+        for i, circle in enumerate(self.Map_qz):
+            index_map_simple[i+1] = circle
+
+        reverse_index_map = { value: key for key, value in index_map_simple.items()}
+
+        return index_map, reverse_index_map
+
+    def compute_risk_cost(self,circle):
+        radius = circle[2]
+        pen_dist_list = np.linspace(0, radius, self.discritization_factor+1)
+        risk_list, length_chord_list = [], []
+        for D in pen_dist_list:
+            delta_theta = abs(2*sympy.acos(D/radius))
+            risk = delta_theta/D  
+            risk_list.append(risk)
+            Length_chord = 2*sympy.sqrt(radius**2 - D**2)
+            length_chord_list.append( Length_chord)
+        
+        risk_list[0] = 10**3
+        return risk_list, pen_dist_list, length_chord_list
